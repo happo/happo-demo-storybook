a221f9856a8dd57b0f4087d951a5e0ac33ea49ef
Henric Trotzig <henric.trotzig@gmail.com>
2019-01-20 10:37:25 +0100

    Remove non-chrome browsers temporarily

--- .happo.js
+++ .happo.js
@@ -9,21 +9,6 @@ module.exports = {
     chrome: new RemoteBrowserTarget('chrome', {
       viewport: '1024x768',
     }),
-    firefox: new RemoteBrowserTarget('firefox', {
-      viewport: '1024x768',
-    }),
-    edge: new RemoteBrowserTarget('edge', {
-      viewport: '1024x768',
-    }),
-    'internet explorer': new RemoteBrowserTarget('internet explorer', {
-      viewport: '1024x768',
-    }),
-    safari: new RemoteBrowserTarget('safari', {
-      viewport: '1024x768',
-    }),
-    'ios-safari': new RemoteBrowserTarget('ios-safari', {
-      viewport: '375x667',
-    }),
   },
 
   plugins: [
